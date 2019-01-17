ffcc5be8977a063e7ddeeb9dc31b4757d52920c1
lengyeltamas <43581652+lengyeltamas@users.noreply.github.com>
2019-01-17 18:08:04 +0100

    change elements

--- week-01/day-4/13_change_element/main.cpp
+++ week-01/day-4/13_change_element/main.cpp
@@ -8,7 +8,11 @@ int main(int argc, char* args[]) {
     // - Change the 8 to 4
     // - Print the fourth element
 
-    
+    int s [6] = { 1, 2, 3, 8, 5, 6 };
+
+    (s[3]) = 4;
+
+    std::cout << "The fourth element of the array is: " << s[3] << std::endl;
 
     return 0;
 }
