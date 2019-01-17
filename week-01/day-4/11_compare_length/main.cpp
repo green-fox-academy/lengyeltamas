4743c747bb065b3634f4ab6136ae88228ec12db9
lengyeltamas <43581652+lengyeltamas@users.noreply.github.com>
2019-01-17 17:23:01 +0100

    day 4 update

--- week-01/day-4/11_compare_length/main.cpp
+++ week-01/day-4/11_compare_length/main.cpp
@@ -9,10 +9,17 @@ int main(int argc, char* args[]) {
     //   with the following content: `[4, 5]`
     // - Print if `p2` has more elements than `p1`
 
-    int p1 [3] = {1,2,3};
-    int p2 [4,5];
+    int p1 [3] = { 1,2,3 };
+    int p2 [2] = { 4,5 };
 
-    if (sizeof(p2) > )
+    int a = sizeof(p1)/sizeof (p1[0]);
+    int b = sizeof(p2)/sizeof (p2[0]);
+
+    if ( a < b ) {
+        std::cout << "p2 array has more elements than p1!" << std::endl;
+    } else {
+        std::cout << "p2 array has less elements than p1!" << std::endl;
+    }
 
     return 0;
 }
