18cb50b316c3dc80f08d75017423858d58b0a98d
lengyeltamas <43581652+lengyeltamas@users.noreply.github.com>
2019-01-18 16:32:11 +0100

    update

--- week-01/day-5/06_ConditionalVariableMutation_day3/main.cpp
+++ week-01/day-5/06_ConditionalVariableMutation_day3/main.cpp
@@ -3,26 +3,36 @@
 
 int main(int argc, char* args[]) {
 
-    double a = 24;
+    int a = 24;
     int out = 0;
     // if a is even increment out by one
 
-    if (a % 2 <= 1){
-        (out += 1)
-    } else {};
+    if ((a%2) == 0) {
+        out++;
+    }
+
     std::cout << out << std::endl;
 
 
+
     int b = 13;
-    std::string out2 = "";
+    std::string out2;
+
     // if b is between 10 and 20 set out2 to "Sweet!"
     // if less than 10 set out2 to "Less!",
     // if more than 20 set out2 to "More!"
 
-    
+    if (b < 20 && b > 10) {
+        out2 = "Sweet!";
+    } else {
+        if (b < 10) {
+           out2 = "Less!";
+        } else {
+            out2 = "More!";
+        }
+    }
 
     std::cout << out2 << std::endl;
-
 
     int c = 123;
     int credits = 100;
@@ -33,12 +43,21 @@ int main(int argc, char* args[]) {
     // and isBonus is false decrement c by 1
     // if isBonus is true c should remain the same
 
+    if (credits >= 50 && isBonus == false) {
+        c +=2;
+    }
+
+    if (credits < 50 && isBonus == false) {
+        c +=1;
+    }
+
     std::cout << c << std::endl;
+
 
 
     int d = 8;
     int time = 120;
-    std::string out3 = "";
+    std::string out3;
     // if d is dividable by 4
     // and time is not more than 200
     // set out3 to "check"
@@ -46,7 +65,12 @@ int main(int argc, char* args[]) {
     // set out3 to "Time out"
     // otherwise set out3 to "Run Forest Run!"
 
+   if ()
+
+
+
     std::cout << out3 << std::endl;
+
 
     return 0;
 }
