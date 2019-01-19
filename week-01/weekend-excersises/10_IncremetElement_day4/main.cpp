fa844ef5d5ece076938938afdf2fcfff78f57aaa
lengyeltamas <43581652+lengyeltamas@users.noreply.github.com>
2019-01-19 21:43:59 +0100

    uploaded IncementElement

---
+++ week-01/weekend-excersises/10_IncremetElement_day4/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* args[]) {
+
+    // - Create an array variable named `t`
+    //    with the following content: `[1, 2, 3, 4, 5]`
+    // - Increment the third element
+    // - Print the third element
+
+    int t [5] = {1, 2, 3, 4, 5};
+
+    (t[2]) = (t[2] + 1);
+
+    std::cout << "The third element of this array is: " << t[2] << std::endl;
+
+    return 0;
+}
