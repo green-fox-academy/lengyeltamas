c9b113744838b5c69a1a849ef2bf9bf5ce55c303
lengyeltamas <43581652+lengyeltamas@users.noreply.github.com>
2019-01-20 13:58:41 +0100

    uploaded sumall

---
+++ week-01/weekend-excersises/17_sumAllElements_day4/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* args[]) {
+
+    // - Create an array variable named `ai`
+    //   with the following content: `[3, 4, 5, 6, 7]`
+    // - Print the sum of the elements in `ai`
+
+
+    int ai [] = {3, 4, 5, 6, 7};
+    int a = sizeof(ai) / sizeof (ai[0]);
+    int sum = 0;
+
+    for (int i = 0; i < a; i++) {
+        sum += ai[i];
+    }
+
+    std::cout << "The array's elements sum are: " << sum << std::endl;
+
+
+    return 0;
+}
