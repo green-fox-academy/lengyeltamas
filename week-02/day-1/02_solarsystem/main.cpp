c82dcfdcbde0a7808851c33c1b44f341d70f5209
lengyeltamas <43581652+lengyeltamas@users.noreply.github.com>
2019-01-21 14:04:33 +0100

    uploading planets

--- week-02/day-1/02_solarsystem/main.cpp
+++ week-02/day-1/02_solarsystem/main.cpp
@@ -2,13 +2,26 @@
 #include <string>
 #include <vector>
 
-std::vector<std::string> putSaturn(const std::vector<std::string>& planets);
+std::vector<std::string> putSaturn(const std::vector<std::string>& planets)
+{
+      std::vector<std::string> temp (planets);
+      temp.insert(temp.begin()+5, 1, "Saturn");
+      return temp;
+}
+
+
 
 int main(int argc, char* args[])
 {
     std::vector<std::string> planets = {"Mercury","Venus","Earth","Mars","Jupiter","Uranus","Neptune"};
-    
-    std::vector<std::string correctPlanets = putSaturn(planets);
+
+    // Saturn is missing from the planetList
+    // Insert it into the correct position
+    // Create a method called putSaturn() which has list parameter and returns the correct list.
+
+    // Expected output: Mercury Venus Earth Mars Jupiter Saturn Uranus Neptune
+
+    std::vector <std::string> correctPlanets = putSaturn(planets);
 
     for(int i = 0; i < correctPlanets.size(); ++i) {
         std::cout << correctPlanets[i] << " ";
@@ -16,9 +29,3 @@ int main(int argc, char* args[])
 
     return 0;
 }
-
-// Saturn is missing from the planetList
-// Insert it into the correct position
-// Create a method called putSaturn() which has list parameter and returns the correct list.
-
-// Expected output: Mercury Venus Earth Mars Jupiter Saturn Uranus Neptune
