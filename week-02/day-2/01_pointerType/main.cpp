2ba16153e5bee978acb93bf37c13fb009ba3f986
lengyeltamas <43581652+lengyeltamas@users.noreply.github.com>
2019-01-22 13:50:24 +0100

    uploading pointertype

--- week-02/day-2/01_pointerType/main.cpp
+++ week-02/day-2/01_pointerType/main.cpp
@@ -13,9 +13,13 @@ int main ()
     double *bpointer = &b;
     std::string *namepointer = &name;
 
-    std::cout << "The memory adress of variable " << a << " is: " << apointer << std::endl;
-    std::cout << "The memory adress of variable " << b << " is: " << bpointer << std::endl;
-    std::cout << "The memory adress of variable " << name << " is: " << namepointer << std::endl;
+    std::cout << "The memory address of variable " << a << " is: " << apointer << std::endl;
+    std::cout << "The memory address of variable " << b << " is: " << bpointer << std::endl;
+    std::cout << "The memory address of variable " << name << " is: " << namepointer << std::endl;
+
+    std::cout << "The value of pointer 'apointer' is: " << *apointer << std::endl;
+    std::cout << "The value of pointer 'bpointer' is: " << *bpointer << std::endl;
+    std::cout << "The value of pointer 'namepointer' is: " << *namepointer << std::endl;
 
     return 0;
 }
