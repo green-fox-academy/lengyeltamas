0b139bdc1120834bd56cfe20bd33afd84110e3c9
lengyeltamas <43581652+lengyeltamas@users.noreply.github.com>
2019-02-26 13:44:49 +0100

    feat(w06d2) finished exercise sandwich

--- week-06/day-2/03_sandwich/main.c
+++ week-06/day-2/03_sandwich/main.c
@@ -12,8 +12,29 @@ The parameters should be:
  - and an integer that represents how many sandwiches you want to order
 */
 
+typedef struct {
+    char* name;
+    float price;
+    float weight;
+} sandwich_t;
+
+float priceOfSandwich(int amount, sandwich_t ham_and_cheese);
+
 int main()
 {
+    sandwich_t ham_and_cheese;
+    ham_and_cheese.name = "Ham and Cheese sandwich";
+    ham_and_cheese.price = 1.2;
+    ham_and_cheese.weight = 0.3;
+
+    int numberOfSandwiches = 9;
+
+    printf("%.2f", priceOfSandwich(numberOfSandwiches, ham_and_cheese));
 
     return 0;
+}
+
+float priceOfSandwich(int amount, sandwich_t ham_and_cheese)
+{
+    return (amount * ham_and_cheese.price);
 }
---
+++ week-06/day-2/04_countLines/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+// Write a function that takes a filename as string,
+// then returns the number of lines the file contains.
+// It should return zero if it can't open the file
+
+int main ()
+{
+    
+    return 0;
+}
