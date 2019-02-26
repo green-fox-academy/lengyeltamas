361f8bbb46230f86fae6c30a78aff31682008755
lengyeltamas <43581652+lengyeltamas@users.noreply.github.com>
2019-02-26 15:09:19 +0100

    feat(w06d2) finished exercise writeMultipleLine

--- week-06/day-2/05_writeSingleLine/main.c
+++ week-06/day-2/05_writeSingleLine/main.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
-// Open a file called "my-file.txt"
-// Write your name in it as a single line
-
 int main ()
 {
+    // Open a file called "my-file.txt"
     FILE *my_file = fopen("../my-file.txt", "w");
     char* my_name = "Lengyel Tamas";
+    // Write your name in it as a single line
     fputs(my_name, my_file);
-    
     return 0;
 }
---
+++ week-06/day-2/06_writeMultipleLines/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+// Create a function that takes 3 parameters: a path, a word and a number
+// and is able to write into a file.
+// The path parameter should be a string that describes the location of the file you wish to modify
+// The word parameter should also be a string that will be written to the file as individual lines
+// The number parameter should describe how many lines the file should have.
+// If the word is "apple" and the number is 5, it should write 5 lines
+// into the file and each line should read "apple"
+
+void fileWriter(char * file_name, char * test_word, int test_num);
+
+int main()
+{
+    char * word = "apple\n";
+    int number_of_lines = 15;
+    fileWriter("../my-file.txt", word, number_of_lines);
+    return 0;
+}
+
+void fileWriter(char * file_name, char * test_word, int test_num)
+{
+    FILE *my_file = fopen(file_name, "w");
+    if (file_name == NULL) {
+        printf("Cannot open file!");
+    }
+    for (int i = 0; i < test_num; ++i) {
+        fputs(test_word, my_file);
+    }
+}
