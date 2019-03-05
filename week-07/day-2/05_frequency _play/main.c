9c13fa6189a7dd70e544a314d58ad2b7d02780dd
lengyeltamas <43581652+lengyeltamas@users.noreply.github.com>
2019-03-05 16:25:46 +0100

    feat(w7d2) 05_frequency_play

--- week-07/day-2/05_frequency _play/main.c
+++ week-07/day-2/05_frequency _play/main.c
@@ -14,23 +14,32 @@
 int main(void) {
 	HAL_Init();
 
-	//BSP_PB_Init(BUTTON_KEY, BUTTON_MODE_GPIO);
+	BSP_PB_Init(BUTTON_KEY, BUTTON_MODE_GPIO);
 
 	BSP_LED_Init(LED1);
 
 	int count = 0;
+	int delay_time = 2000;
+	int increasing = 1;
 
 	while (1) {
-		if (BSP_PB_GetState(BUTTON_KEY) && count < 5 ) {
-			count++;
-			HAL_Delay(250);
-		} else if (BSP_PB_GetState(BUTTON_KEY) && count >= 5 ){
-			while (1) {
-			        BSP_LED_On(LED1);
-			        HAL_Delay(500);
-			        BSP_LED_Off(LED1);
-			        HAL_Delay(500);
-			}
-		}
+		 BSP_LED_On(LED1);
+		 HAL_Delay(delay_time);
+		 BSP_LED_Off(LED1);
+		 HAL_Delay(delay_time);
+		 count++;
+		 if(count == 3) {
+			 count = 0;
+			 if(increasing) {
+				 delay_time /= 2;
+			 } else {
+				 delay_time *= 2;
+			 }
+		 }
+		 if (delay_time == 250) {
+			 increasing = 0;
+		 } else if (delay_time == 2000) {
+			 increasing = 1;
+		 }
 	}
 }
